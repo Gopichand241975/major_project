90761c626e017831333c1e0bf388387c906d8a16
Gopichand241975 <gopichand241975@gmail.com>
2026-07-26 17:15:14 +0530

    add DeepSORT tracker

--- tracker.py
+++ tracker.py
@@ -8,3 +8,14 @@ def _in_zone(bbox, zone):
     zx1, zy1, zx2, zy2 = zone
     cx, cy = (x1 + x2) / 2, (y1 + y2) / 2
     return zx1 <= cx <= zx2 and zy1 <= cy <= zy2
+
+class PersonTracker:
+    def __init__(self):
+        self.tracker = DeepSort(max_age=30)
+        self.zone_entry_time = {}
+
+    def update(self, detections, frame):
+        ds_input = [([d["bbox"][0], d["bbox"][1],
+                      d["bbox"][2] - d["bbox"][0], d["bbox"][3] - d["bbox"][1]],
+                     d["conf"], "person") for d in detections]
+        tracks = self.tracker.update_tracks(ds_input, frame=frame)
