b8d230afa4e6f263de3036785692f2f85af1d73c
Gopichand241975 <gopichand241975@gmail.com>
2026-08-02 12:33:01 +0530

    complted alert.py

--- alerts.py
+++ alerts.py
@@ -4,7 +4,7 @@ import cv2
 import requests
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
-TAMPER_BLACKOUT_THRESHOLD = 15   # mean pixel intensity below this = likely covered/blocked
+TAMPER_BLACKOUT_THRESHOLD = 15   # mean pixel intensity below this = likely covered/block
 TAMPER_DURATION_SECONDS = 3      # must persist this long to avoid false alarms
 
 
